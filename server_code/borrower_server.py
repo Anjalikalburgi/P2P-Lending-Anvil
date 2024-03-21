73b6bf92d2d36a676fa5f5f4829200f0dc0ae9d4
anjalikalburgi9@gmail.com <anjalikalburgi9@gmail.com>
2024-03-21 12:19:37 +0000

    Edited borrower_server

--- server_code/borrower_server.py
+++ server_code/borrower_server.py
@@ -384,19 +384,19 @@ def get_user_points(id):
                     print("Spouse profession:", spouse_profession_points)
                     user_points += spouse_profession_points
 
-        loan_points_total = 0
-        for loan_column in loan_columns:
-            loan_value = user[loan_column].lower()
-            if loan_value in ['yes', 'no']: 
-                loan_search = app_tables.fin_admin_beseem_categories.search(
-                    (app_tables.fin_admin_beseem_categories.group_name == 'all_loans') &
-                    (app_tables.fin_admin_beseem_categories.sub_category == loan_column.lower()) &
-                    (app_tables.fin_admin_beseem_categories.is_liveloan == loan_value.lower())
-                )
-                if loan_search:
-                    loan_points_total += loan_search[0]['min_points']
+        # loan_points_total = 0
+        # for loan_column in loan_columns:
+        #     loan_value = user[loan_column].lower()
+        #     if loan_value in ['yes', 'no']: 
+        #         loan_search = app_tables.fin_admin_beseem_categories.search(
+        #             (app_tables.fin_admin_beseem_categories.group_name == 'all_loans') &
+        #             (app_tables.fin_admin_beseem_categories.sub_category == loan_column.lower()) &
+        #             (app_tables.fin_admin_beseem_categories.is_liveloan == loan_value.lower())
+        #         )
+        #         if loan_search:
+        #             loan_points_total += loan_search[0]['min_points']
 
-        user_points += loan_points_total
+        # user_points += loan_points_total
 
         # if home_loan:
         #     home_loan_search = app_tables.fin_admin_beseem_categories.search(group_name='all_loans', sub_category=home_loan.lower())
